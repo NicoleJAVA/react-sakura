32da3c6306f9721999d6d10bd451b7e82a7690a5
Nicole Chang <powernicole2@yahoo.com.tw>
2022-05-06 18:40:51 +0800

    feat: login and token handling

--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -2,12 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Card, Input, Icon, Button, message} from 'antd';
 import axios from 'axios';
+import ApiUrl from '../config/api_url';
 
 export default class Login extends React.Component{
 
   constructor(props) {
     super(props);
+    this.state = {
+      userName: '',
+      password: '',
+    };
   }
+
+  checkLogin = () => {
+    if (this.isBlank(this.state.userName)) {
+      message.error('帳號不能為空');
+      return false;
+    } else if (this.isBlank(this.state.password)) {
+      message.error('密碼不能為空');
+      return false;
+    }
+
+
+    let dataProps = {
+      'username': this.state.userName,
+      'password': this.state.password,
+    };
+    console.log('dataProps', dataProps); // todo dele
+
+    axios({
+      method: 'post',
+      url: ApiUrl.USER_LOGIN,
+      data: dataProps,
+    }).then(res => {
+      console.log(res.data);
+      let data = res.data.data;
+      if (res.data.code === 0) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('userId', data.id);
+        localStorage.setItem('username', data.username );
+      } else {
+        message.error('帳號或密碼錯誤');
+      }
+    });
+  };
+
+   isBlank = function(str ){
+    return (!!!str || /^\s*$/.test(str));
+  }
+
+  setUserName = (name) => {
+    this.setState({
+      userName: name,
+    });
+  };
+
+  setPassword = (password) => {
+    this.setState({
+      password: password,
+    });
+  };
 
   render(){
       return(
@@ -16,8 +70,9 @@ export default class Login extends React.Component{
             <Input
               id="userName"
               size="large"
-              placeholder='請輸入使用者名稱'
+              placeholder='請輸入帳號'
               prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0, 0.25)'}}/>}
+              onChange={(e) => {this.setUserName(e.target.value)}}
             />
             <br/><br/>
             <Input.Password
@@ -25,9 +80,10 @@ export default class Login extends React.Component{
               size="large"
               placeholder='請輸入密碼'
               prefix={<Icon type="key" style={{color: 'rgba(0, 0, 0, 0.25)'}}/>}
+              onChange={(e) => {this.setPassword(e.target.value)}}
             />
             <br/><br/>
-            <Button type='primary' size="large" block>Login</Button>
+            <Button type='primary' size="large" block onClick={this.checkLogin}>Login</Button>
           </Card>
         </div>
       );
