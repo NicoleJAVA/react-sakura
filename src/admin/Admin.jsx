5c1bd0d042e067c518e98414b5b221b255c7686f
Nicole Chang <powernicole2@yahoo.com.tw>
2022-05-07 19:40:03 +0800

    feat: Admin page menu UI layout (Lecture 10)

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -17,10 +17,64 @@ export default class Admin extends React.Component{
 
   render(){
       return(
-          <Layout>
+          <Layout className="admin-menu-layout">
               {/* left side menu */}
               <Sider>
+                  <div className="menu-title fs-xl">商城後台管理系統</div>
+                  <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                      <Menu.Item key="1">
+                          <Icon type="pie-chart"/>
+                          <span>首頁</span>
+                      </Menu.Item>
+                 
 
+                  <SubMenu
+                    key="/user"
+                    title={
+                        <span>
+                            <Icon type="desktop"/>
+                            <span>帳號管理</span>
+                        </span>
+                    }
+                  >
+
+                    <Menu.Item key="userList">帳號列表</Menu.Item> 
+                  </SubMenu>
+
+                  <SubMenu
+                    key="/product"
+                    title={
+                        <span>
+                            <Icon type="desktop"/>
+                            <span>商品管理</span>
+                        </span>
+                    }
+                  >
+
+                    <Menu.Item key="productList">商品列表</Menu.Item> 
+                    <Menu.Item key="addProduct">新增商品</Menu.Item> 
+                  </SubMenu>
+
+                  <SubMenu
+                    key="/category"
+                    title={
+                        <span>
+                            <Icon type="desktop"/>
+                            <span>分類管理</span>
+                        </span>
+                    }
+                  >
+
+                    <Menu.Item key="categoryList">
+                      <Icon type="file"/>
+                        <span>分類列表</span> 
+                    </Menu.Item> 
+                    <Menu.Item key="addCategory">
+                      <Icon type="file"/>
+                        <span>新增分類</span> 
+                    </Menu.Item> 
+                  </SubMenu>
+                </Menu>
               </Sider>
 
               {/* right side: main content */}
