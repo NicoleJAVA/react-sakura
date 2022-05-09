8f91252afd5da9ab82f6624222e481781e3e3550
Nicole Chang <powernicole2@yahoo.com.tw>
2022-05-09 17:24:22 +0800

    feat: render user list (lecture 13)

--- src/user/UserList.jsx
+++ src/user/UserList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {List, Avatar, Button, Row, Col} from 'antd';
 import axios from 'axios';
 import ApiUrl from '../config/api_url';
 import TokenHeaders from '../utils/tokenUtils';
@@ -29,9 +30,62 @@ export default class UserList extends React.Component{
   };
 
   render(){
-      let {list} = this.state;
+      let {userList} = this.state;
+
       return(
-        <div>UserList</div>
+        <div>
+            <List
+                header={
+                    <Row className="user-list-row">
+                        <Col span={2}>
+                            <b>大頭貼</b>
+                        </Col>
+                        <Col span={4}>
+                            <b>帳號名稱</b>
+                        </Col>
+                        <Col span={4}>
+                            <b>電話</b>
+                        </Col>
+                        <Col span={6}>
+                            <b>地址</b>
+                        </Col>
+                        <Col span={3}>
+                            <b>操作</b>
+                        </Col>
+                    </Row>
+                }
+                bordered
+                dataSource={userList}
+                renderItem={item => (
+                    <List.Item>
+                        <Row className='user-list-row'
+                        >
+                            <Col span={2}>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar src={item.head_image}/>
+                                    }
+                                />
+                            </Col>
+                            <Col span={4}>
+                                {item.username}
+                            </Col>
+                            <Col span={4}>
+                                {item.mobile}
+                            </Col>
+                            <Col span={6}>
+                                {item.address}
+                            </Col>
+                            <Col span={3}>
+                                <Button type="primary">編輯</Button>
+                                <Button type="primary">刪除</Button>
+                            </Col>
+                        </Row>
+                    </List.Item>
+                )}
+            >
+            </List>
+        </div>
       );
   }
 }
