2b0f2cb6f5fbf383a5ceb85047997686af1246a1
Nicole Chang <powernicole2@yahoo.com.tw>
2022-05-11 19:08:37 +0800

    feat: initialize category parent list (lecture 18)

--- src/category/AddCategory.jsx
+++ src/category/AddCategory.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {List, Avatar, Button, Row, Col, Select, Card, Input, Upload, Dividers} from 'antd';
-import {PlusOutlined} from '@ant-design/icons';
+import axios from 'axios';
+import ApiUrl from '../config/api_url';
 import TokenHeaders from '../utils/tokenUtils';
+import {List, Avatar, Button, Row, Col, Select, Card, Input, Upload, Dividers, Form} from 'antd';
+import {PlusOutlined} from '@ant-design/icons';
 
 const {Option} = Select;
 
@@ -11,7 +13,7 @@ export default class AddCategory extends React.Component{
     super(props);
     this.state = {
         categoryList: [],
-        modalVisible: false,
+        modelVisible: false,
         id: '',
         image: '',
         name: '',
@@ -22,13 +24,44 @@ export default class AddCategory extends React.Component{
 
   componentDidMount() {
       this._isMounted = true;
+      this.getCategoryList();
   }
 
   componentWillUnmount() {
     this._isMounted = false;
 }
 
+    getCategoryList = () => {
+        axios({
+            method: "get",
+            url: ApiUrl.CATEGORY_ALL,
+            headers: TokenHeaders,
+        }).then(res => {
+        let data = res.data.data;
+
+        if (this._isMounted) {
+            this.setState({
+                categoryList: data,
+            });
+        }
+        });
+    };
+
   render(){
+      let {categoryList} = this.state;
+      let parentList = [];
+      categoryList.forEach((item, index) => {
+          if (this.state.level === 'V2') {
+              if (item.level === 'V1') {
+                  parentList.push(item);
+              }
+          } else if (this.state.level === 'V1') {
+            if (item.level === 'V0') {
+                parentList.push(item);
+            }
+        }
+      });
+
       const uploadButton = (
           <div>
               <PlusOutlined/>
@@ -37,7 +70,19 @@ export default class AddCategory extends React.Component{
       );
       return(
         <div className="category-layout">
-          <Card></Card>
+          <Card>
+              <Form>
+                  <Form.Item label="父類別名稱">
+                      <Select className='add-category-select' defaultValue={this.state.pid} value={this.state.pid}>
+                          {
+                              parentList.map(item => (
+                                  <Option key ={item.id} value={item.id}>{item.name}</Option>
+                              ))
+                          }
+                      </Select>
+                  </Form.Item>
+              </Form>
+          </Card>
         </div>
       );
   }
